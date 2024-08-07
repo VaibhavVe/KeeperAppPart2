7db5488206961111b78aabeb3dd6395c8479c460
VaibhavVe <122148379+VaibhavVe@users.noreply.github.com>
2024-08-07 11:10:09 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+import notes from "./notes";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
